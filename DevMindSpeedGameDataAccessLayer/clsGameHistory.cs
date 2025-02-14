e73a5fa8c1d25418eb3e5c190850bb3d2bab1171
ZakariaR2000 <162996600+ZakariaR2000@users.noreply.github.com>
2025-02-14 15:34:15 +0300

    Add files via upload

---
+++ DevMindSpeedGameDataAccessLayer/clsGameHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMindSpeedGameDataAccessLayer
+{
+    public class clsGameHistory
+    {
+        public int GameHistoryID { get; set; }
+        public Guid GameSessionID { get; set; }
+        public string Question { get; set; }
+        public float Answer { get; set; }
+        public bool Correct { get; set; }
+        public float TimeTaken { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
